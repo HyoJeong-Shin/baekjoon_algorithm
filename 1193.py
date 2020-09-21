dd4636a84aaf2ca0515611b8872980d337d07b70
HyoJeong-Shin <kyn05298@naver.com>
2020-09-21 17:13:45 +0900

    수학1 study

--- 1193.py
+++ 1193.py
@@ -21,3 +21,53 @@ while True:
             break
     plus +=1
         
+
+
+'''
+스터디 해설 1
+# 01. 배열이 하나씩 늘어남
+# 02. 홀수번 배열 : 분자 감소, 분모 증가
+# 03. 짝수번 배열 : 분자 증가, 분모 감소
+
+num = int(input())
+up = []   #     분모
+down = [] #     분자
+
+for i in range(1, num+1):
+    if i % 2 == 0:
+        # 짝수일 경우
+        up += [j for j in range(1, i+1)]
+        down += [j for j in range(i, 0, -1)]
+    else:
+        # 홀수일 경우
+        down += [j for j in range(1, i+1)]
+        up += [j for j in range(i, 0, -1)]
+
+    if len(up) >= num:
+        print(str(up[num-1])+'/'+str(down[num-1]))
+        break
+
+스터디 해설 2
+X = int(input())
+
+#X가 몇번째 대각선인지 찾아보자!
+line = 1
+line_sum = 0
+pre_line_sum = 0
+while True:
+    pre_line_sum = line_sum
+    line_sum+=line
+    
+    if line_sum >= X :
+        break
+
+    line+=1
+
+X_num = X-pre_line_sum #X가 대각선 라인에서 몇번째에 위치하는지 찾아봄!
+rev_X_num = line_sum+1-X
+
+if line%2==0 :
+    print(X_num,rev_X_num,sep="/") 
+else :
+    print(rev_X_num,X_num,sep="/")
+'''
